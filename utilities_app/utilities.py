6e7f69023df553ee9500b800b1d3a75f9b7ad357
Andrew Ludwar <andrew.ludwar@gmail.com>
2019-12-24 23:49:51 -0700

    Trying wtforms

--- utilities_app/utilities.py
+++ utilities_app/utilities.py
@@ -1,17 +1,24 @@
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def my_form():
-    return render_template('index.html')
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
 
-@app.route('/', methods=['POST'])
-def index_post():
-    EnmaxField1 = request.form['Enmax.field1']
-    EnmaxField2 = request.form['Enmax.field2']
-    EnmaxField3 = request.form['Enmax.field3']
-    fields = (EnmaxField1 + EnmaxField2 + EnmaxField3)
-    return fields
-
-
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = (form.username.data + form.email.data + form.password.data)
+        #db_session.add(user)
+        #flash('Thanks for registering')
+        #return redirect(url_for('login'))
+    return render_template('register.html', form=form)
