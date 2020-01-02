6278ca14a9b87e4794f824aa0e08d50c71140141
Andrew Ludwar <andrew.ludwar@gmail.com>
2020-01-02 16:58:19 -0700

    Adding dockerfile directory for easy deployment reference

--- utilities_app/README.md
+++ utilities_app/README.md
@@ -4,6 +4,10 @@ This is a very basic python web form, utilizing flask, to insert monthly utiliti
 
 I realise this is pretty rudimentary and probably could be accomplished in a simpler way using another API/method, but I wanted to experiment. With more regular python practice, I expect I'll look back at this code and laugh/cringe.
 
+
+Documented in the __docker__ folder, I'm using a pre-built (and likely very insecure) [docker image](https://hub.docker.com/r/tiangolo/uwsgi-nginx-flask) from docker hub that has nginx, uwsgi, and flask built into it. I'll eventually rebuild this with RHEL8 ubi-minimal.
+
+
 Here's an example:
 
 ![alt text](https://raw.githubusercontent.com/aludwar/python/master/utilities_app/utilities.png "Example utilities graph")
---
+++ utilities_app/docker/app/main.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python3
+
+import subprocess, datetime, time
+from datetime import timedelta
+from decimal import *
+from influxdb import InfluxDBClient
+from flask import Flask, request, render_template, url_for, flash, redirect
+
+host = '<hostname>'
+port = '8086'
+user = 'admin'
+password = '<password>'
+dbname = 'utilities_new'
+
+app = Flask(__name__)
+
+def subtract_one_month(dt0):
+    dt1 = dt0.replace(day=1)
+    dt2 = dt1 - timedelta(days=1)
+    dt3 = dt2.replace(day=1)
+    return dt3
+
+def get_enmax_floats(f1,f2,f3,f4,f5):
+  try:
+    getcontext().prec = 2
+    n1 = float(f1)
+    n2 = float(f2)
+    n3 = float(f3)
+    n4 = float(f4)
+    n5 = float(f5)
+  except ValueError:
+    print("Input should be numbers. Try again.")
+    return 500
+  else:
+    return n1, n2, n3, n4, n5
+
+def get_cui_floats(f1,f2,f3,f4,f5,f6,f7,f8,f9):
+  try:
+    n1 = float(f1)
+    n2 = float(f2)
+    n3 = float(f3)
+    n4 = float(f4)
+    n5 = float(f5)
+    n6 = float(f6)
+    n7 = float(f7)
+    n8 = float(f8)
+    n9 = float(f9)
+  except ValueError:
+    print("Input should be numbers. Try again.")
+    return 500
+  else:
+    return n1, n2, n3, n4, n5, n6, n7, n8, n9
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/", methods=['POST'])
+def index_post():
+    provider = str(request.form['target'])
+
+    if provider == 'Enmax':
+      EnmaxEusage = request.form['Enmax.eusage']
+      EnmaxNGusage = request.form['Enmax.ngusage']
+      EnmaxEcost= request.form['Enmax.ecost']
+      EnmaxNGcost = request.form['Enmax.ngcost']
+      EnmaxTcost = request.form['Enmax.tcost']
+      EnmaxYear = str(subtract_one_month(datetime.datetime.now()))
+      EnmaxYear = int(EnmaxYear[0:4])
+
+      try:
+        EnmaxMonth = int(request.form['emonth'])
+      except ValueError:
+        print("Please select a month")
+        return 'Please select a month'
+
+      dt = datetime.datetime(year=EnmaxYear, month=EnmaxMonth, day=15)
+      EnmaxEpoch = int(time.mktime(dt.timetuple())) * int(1000000000)
+      utilities_tuple = get_enmax_floats(EnmaxEusage, EnmaxNGusage, EnmaxEcost, EnmaxNGcost, EnmaxTcost)
+      if utilities_tuple == 500:
+        return 'Input should be numbers. Try again.'
+      else:
+        dbclient = InfluxDBClient(host, port, user, password, dbname)
+        
+        json_body = [
+        {
+            "measurement": "utilities",
+            "tags": {
+                "vendor": "enmax"
+            },
+            "time": EnmaxEpoch,
+            "fields": {
+                "electricity_usage": (utilities_tuple[0]),
+                "natural_gas_usage": (utilities_tuple[1]),
+                "electrical_cost": (utilities_tuple[2]),
+                "natural_gas_cost": (utilities_tuple[3]),
+                "carbon_levy": 0.0,
+                "total_cost": (utilities_tuple[4]),
+            }
+        }
+        ]
+        
+        dbclient.write_points(json_body)
+
+        return 'OK'
+
+    elif provider == 'CUI':
+      CUIgarbage = request.form['CUI.garbage']
+      CUIrecycle = request.form['CUI.recycle']
+      CUIrstorm = request.form['CUI.rstorm']
+      CUIrsewer = request.form['CUI.rsewer']
+      CUIrwater = request.form['CUI.rwater']
+      CUIwusage = request.form['CUI.wusage']
+      CUIwcost = request.form['CUI.wcost']
+      CUIscost = request.form['CUI.scost']
+      CUItcost = request.form['CUI.tcost']
+      CUIYear = str(subtract_one_month(datetime.datetime.now()))
+      CUIYear = int(CUIYear[0:4])
+
+      try:
+        CUIMonth = int(request.form['cuimonth'])
+      except ValueError:
+        print("Please select a month")
+        return 'Please select a month'
+
+      dt = datetime.datetime(year=CUIYear, month=CUIMonth, day=15)
+      CUIEpoch = int(time.mktime(dt.timetuple())) * int(1000000000)
+      utilities_tuple = get_cui_floats(CUIgarbage, CUIrecycle, CUIrstorm, CUIrsewer, CUIrwater, CUIwusage, CUIwcost, CUIscost, CUItcost)
+      if utilities_tuple == 500:
+        return 'Input should be numbers. Try again.'
+      else:
+        dbclient = InfluxDBClient(host, port, user, password, dbname)
+        
+        json_body = [
+        {
+            "measurement": "utilities",
+            "tags": {
+                "vendor": "cui"
+            },
+            "time": CUIEpoch,
+            "fields": {
+                "garbage": (utilities_tuple[0]),
+                "lifecycle": 0.0,
+                "recycle_centre": (utilities_tuple[1]),
+                "res_storm": (utilities_tuple[2]),
+                "res_sewer": (utilities_tuple[3]),
+                "res_water": (utilities_tuple[4]),
+                "water_usage": (utilities_tuple[5]),
+                "water_consume_charge": (utilities_tuple[6]),
+                "sewer_consume_charge": (utilities_tuple[7]),
+                "total_cost": (utilities_tuple[8]),
+            }
+        }
+        ]
+        
+        dbclient.write_points(json_body)
+
+        return 'OK'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=80)
